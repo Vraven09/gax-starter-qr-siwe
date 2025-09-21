9c216a034d8f6e02c2e338f74f6d40f5478ce8ac
Victor Rodriguez <galaxiagax@gmail.com>
2025-09-21 21:37:31 +0200

    Create staking.jsx

---
+++ pages/staking.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Staking() {
+  return (
+    <div className="container">
+      <Head><title>Staking | GAX</title></Head>
+      <h1>Staking (placeholder)</h1>
+      <p>Aquí conectaremos tu contrato en Polygon Amoy: aprobar GAX, stake/withdraw, earned.</p>
+    </div>
+  )
+}
